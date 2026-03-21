bcfbb1bf50bd6622413ca19d6ad1ce92a170bb90
d-laub <dlaub@ucsd.edu>
2026-03-21 15:46:35 -0700

    update to just use dlaub-togo as-is

--- setup-shell.sh
+++ setup-shell.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
-# Mirrors https://github.com/d-laub/dlaub-togo/blob/main/setup_bash.sh for image build.
-
 set -euo pipefail
 
-# Pixi install.sh only updates bashrc when SHELL is bash (often /bin/sh in Docker builds).
+# Pixi install.sh and several tools expect bash.
 export SHELL=/bin/bash
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" "" --unattended
+dlaub_togo_dir=$(mktemp -d)
+trap 'rm -rf "${dlaub_togo_dir}"' EXIT
 
-curl -fsSL https://raw.githubusercontent.com/d-laub/dlaub-togo/main/aliases.sh >> "${HOME}/.bash_aliases"
+git clone --depth 1 --branch main https://github.com/d-laub/dlaub-togo.git "${dlaub_togo_dir}"
 
-git clone --depth 1 --branch main https://github.com/d-laub/dlaub-togo.git /tmp/dlaub-togo-tmp
-mkdir -p "${HOME}/.oh-my-bash/themes"
-cp -r /tmp/dlaub-togo-tmp/agnoster-multiline "${HOME}/.oh-my-bash/themes/"
-rm -rf /tmp/dlaub-togo-tmp
+cd "${dlaub_togo_dir}"
 
-curl -fsSL https://pixi.sh/install.sh | sh
-export PATH="${HOME}/.pixi/bin:${PATH}"
-pixi g i ripgrep bat glow-md sd zoxide rnr fd-find exa prek git gh
+bash setup_bash.sh
 
-echo 'eval "$(zoxide init bash)"' >> "${HOME}/.bashrc"
-sd '^OSH_THEME=.*$' 'OSH_THEME="agnoster-multiline"' "${HOME}/.bashrc"
+cd /
